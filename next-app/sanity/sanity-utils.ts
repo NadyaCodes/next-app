77fd2d7449717b59a66c95708cd8b326f1857949
NadyaCodes <nadyacorscadden@gmail.com>
2023-10-08 15:39:02 -0400

    add tailwind and basic styling

--- next-app/sanity/sanity-utils.ts
+++ next-app/sanity/sanity-utils.ts
@@ -1,6 +1,7 @@
 import { createClient, groq } from "next-sanity";
+import type { Project } from "@/types/Project";
 
-export async function getProjects() {
+export async function getProjects(): Promise<Project[]> {
   const client = createClient({  projectId: "2g4p5bpj",
   dataset: "production",
   apiVersion: "2023-10-07"});
